d1e080027d23a16a2760b8c6886246a22e602496
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-05-18 21:13:14 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,39 +1,8 @@
 #include <stdio.h>
 #include <assert.h>
 
-
-typedef enum
-{
-  LOW_Temp_BREACH ,
-  LOW_Temp_WARNING,
-  Temp_NORMAL,
-  HIGH_Temp_WARNING,
-  HIGH_Temp_BREACH
- }TempRange_e;
-
-typedef enum
-{
-  LOW_SOC_BREACH ,
-  LOW_SOC_WARNING,
-  SOC_NORMAL,
-  HIGH_SOC_WARNING,
-  HIGH_SOC_BREACH
- }SOCRange_e;
-
-typedef enum
-{
-  ChargeRate_BREACH ,
-  ChargeRate_WARNING,
-  ChargeRate_NORMAL
- }ChargeRateRange_e;
- 
-
-
 typedef struct
 {
-    TempRange_e TempRange ;
-    SOCRange_e SOCRange ;
-    ChargeRateRange_e ChargeRateRange ;
     int TempBreachAlerter;
     int SOCBreachAlerter;
     int ChargeRateBreachAlerter;
@@ -42,19 +11,20 @@ typedef struct
     int ChargeRateWarningAlerter;
 }BMS_s;
 
-BMS_s BatteryCheck_s ={
-    Temp_NORMAL,
-    SOC_NORMAL,
-    ChargeRate_NORMAL,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0
-};
-
- _Bool CheckTempInRange(float temperature)
+BMS_s BatteryCheck_s ={0, 0,0,0,0,0};
+   _Bool CheckTempInWarningRange(float temperature)
+  {
+      if((0<= temperature <=2.25) ||
+            (42.75<= temperature <=45))
+      {
+          return 1;
+      }
+      else
+      {
+        return 0;  
+      }
+  }
+   _Bool CheckTempInRange(float temperature)
   {
       if(temperature < 0|| temperature > 45)
       {
@@ -65,16 +35,17 @@ BMS_s BatteryCheck_s ={
         return 1;  
       }
   }
-   _Bool CheckTempInWarningRange(float temperature)
+
+_Bool CheckSOCInWarningRange(float soc)
   {
-      if((0<= temperature <=2.25) ||
-            (42.75<= temperature <=45))
+      if((10<= soc <=24) ||
+            (76<= soc <=80))
       {
-          return 0;
+          return 1;
       }
       else
       {
-        return 1;  
+        return 0;  
       }
   }
   
@@ -90,16 +61,15 @@ BMS_s BatteryCheck_s ={
       }
       
   }
- _Bool CheckSOCInWarningRange(float soc)
+    _Bool CheckChargeRateInWarningRange(float chargeRate)
   {
-      if((10<= soc <=24) ||
-            (76<= soc <=80))
+      if(0.76<=chargeRate <= 0.8)
       {
-          return 0;
+          return 1;
       }
       else
       {
-        return 1;  
+        return 0;  
       }
   }
 _Bool CheckChargeRateInRange(float chargeRate)
@@ -113,78 +83,60 @@ _Bool CheckChargeRateInRange(float chargeRate)
         return 1;  
       }
   }
-   _Bool CheckChargeRateInWarningRange(float chargeRate)
-  {
-      if(0.76<=chargeRate <= 0.8)
-      {
-          return 0;
-      }
-      else
-      {
-        return 1;  
-      }
-  }
+
    void TempWarningAlerter(void)
    {
        BatteryCheck_s.TempWarningAlerter ++;
-       printf("Temperature nearing out of range Warning!\n");
+       printf("Warning :Temperature nearing out of range !\n");
    }
   void TempAlerter(void)
   {
      BatteryCheck_s.TempBreachAlerter++;
-     printf("Temperature out of range !\n");
+     printf("Error:Temperature out of range !\n");
   }
   void ChargeRateWarningAlerter(void)
    {
-       BatteryCheck_s.TempWarningAlerter ++;
-       printf("Temperature nearing out of range Warning!\n");
+       BatteryCheck_s.ChargeRateWarningAlerter ++;
+       printf("Warning :ChargeRate nearing out of range !\n");
    }
   void SOCAlerter(void)
   {
-     BatteryCheck_s.TempBreachAlerter++;
-     printf("Temperature out of range !\n");
+     BatteryCheck_s.SOCBreachAlerter++;
+     printf("Error:SOC out of range !\n");
   }
   void SOCWarningAlerter(void)
    {
-       BatteryCheck_s.TempWarningAlerter ++;
-       printf("Temperature nearing out of range Warning!\n");
+       BatteryCheck_s.SOCWarningAlerter ++;
+       printf("Warning: SOC nearing out of range !\n");
    }
   void ChargeRateAlerter(void)
   {
-     BatteryCheck_s.TempBreachAlerter++;
-     printf("Temperature out of range !\n");
+     BatteryCheck_s.ChargeRateBreachAlerter++;
+     printf("Error:ChargeRate out of range !\n");
   }
  
   void BatteryTempIsOk(float temperature)
   {
       if(CheckTempInWarningRange(temperature) == 1)
      {
-         SOCWarningAlerter();
-
+         TempWarningAlerter();
      }
      else if(CheckTempInRange(temperature) == 0)
      {
-       SOCAlerter();
-
+       TempAlerter();
      }
-     else
-     {
-
-     }
-     
+     else{   }
   }
   
   void BatterySOCIsOk(float soc)
   {
      if(CheckSOCInWarningRange(soc) == 1)
      {
-         SOCAlerter();
-
+         SOCWarningAlerter();
      }
      else if(CheckSOCInRange(soc) == 0)
      {
        SOCAlerter();
-
      }
      else
      {
@@ -197,19 +149,16 @@ _Bool CheckChargeRateInRange(float chargeRate)
      if(CheckChargeRateInWarningRange(chargeRate) == 1)
      {
          ChargeRateWarningAlerter();
-
      }
      else if(CheckChargeRateInRange(chargeRate) == 0)
      {
        ChargeRateAlerter();
-
      }
      else
      {
 
      }
   }
-
 
 void BatteryIsOk(float temperature, float soc, float chargeRate) 
 {
@@ -222,8 +171,9 @@ void BatteryIsOk(float temperature, float soc, float chargeRate)
 
 int main() {
     BatteryIsOk(25, 70, 0.7);
-
+    // BatteryIsOk(46, 81, 1.0);
+    // BatteryIsOk(-1, 10, 0.9);
+    // BatteryIsOk(1, 22, 0.78);
+    // BatteryIsOk(44, 77, 2.0);
     
-    BatteryIsOk(50, 85, 0);
-
 }
